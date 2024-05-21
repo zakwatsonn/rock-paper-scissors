fbeb1d49a38bd7eb43fbf7f62c2c5d3d84deddb6
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2024-05-21 19:34:01 +0100

    started function for playRound

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+let playerScore = 0;
+let compScore = 0;
+
 //function for initialising computer choice
 function getComputerChoice() {
     let randomiser = Math.random() ;
-    
+    let computer = '';
     //make choice based on randomiser
     if (randomiser <= 1/3) {
-        return ('rock');
-    } else if (randomiser >= 1/3 && randomiser <= 1) {
-        return ('paper');
+        computer = 'rock';;
+    } else if (randomiser > 1/3 && randomiser <= 1) {
+        computer = 'paper';
     } else {
-        return ('scissors');
+        computer = 'scissors';
     }
+    return computer;
 }
 
 //function for acquiring user choice
 function getPlayerChoice() {
     let player = prompt('Enter your choice');
     return player.toLowerCase();
+}
+
+//round function
+function playRound(pChoice, cChoice) {
+    
 }
